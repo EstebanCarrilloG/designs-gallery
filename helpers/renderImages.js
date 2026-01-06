22058c716a26f5f37794bbd1898eb234e258a308
EstebanCarrilloG <estexav0996@gmail.com>
2026-01-06 09:36:27 -0500

    Add captions to gallery images and improve styles

--- helpers/renderImages.js
+++ helpers/renderImages.js
@@ -2,23 +2,24 @@ import imgsDb from "../database/imgsDb.js";
 
 function renderImages() {
   imgsDb.forEach((e, i) => {
-
     let imgContainer = document.createElement("div");
-    
+
     imgContainer.classList.add("img-container");
 
-    let rndNumber = getRandomInt(-5, 5);
-
     let img = document.createElement("img");
-        
+
     img.className = "gallery-img";
     img.id = i;
     img.src = e.src;
     img.alt = e.alt;
     img.height = e.height ? e.height : "300";
-    img.style.transform = `rotate(${rndNumber}deg)`;
 
     imgContainer.appendChild(img);
+
+    let caption = document.createElement("div");
+    caption.className = "caption";
+    caption.textContent = e.alt ? e.alt : "Sin título";
+    imgContainer.appendChild(caption);
 
     setTimeout(function () {
       galeria.appendChild(imgContainer);
